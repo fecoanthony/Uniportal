ada6459c18c2005e2f9af2c61ebae55111d49907
anthony <fecotony@gmail.com>
2025-11-08 14:57:33 +0100

    result model modified

--- backend/models/Result.Model.js
+++ backend/models/Result.Model.js
@@ -12,5 +12,16 @@ const resultSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+// Optional: Add method to auto-calculate grade + GPA
+ResultSchema.pre("save", function (next) {
+  if (this.score >= 70) (this.grade = "A"), (this.gpa = 5.0);
+  else if (this.score >= 60) (this.grade = "B"), (this.gpa = 4.0);
+  else if (this.score >= 50) (this.grade = "C"), (this.gpa = 3.0);
+  else if (this.score >= 45) (this.grade = "D"), (this.gpa = 2.0);
+  else if (this.score >= 40) (this.grade = "E"), (this.gpa = 1.0);
+  else (this.grade = "F"), (this.gpa = 0.0);
+  next();
+});
+
 const Result = mongoose.model("Result", resultSchema);
 export default Result;
