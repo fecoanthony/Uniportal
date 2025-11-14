6d63eb5aced2ede0811e9458df97d539cedd3bad
anthony <fecotony@gmail.com>
2025-11-14 23:45:49 +0100

    zustand

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -4,13 +4,6 @@ import { create } from "zustand";
 const useAuthStore = create((set) => ({
   user: null,
   token: null,
-
-  // login: (user, token) => {
-  //   set({ user, token });
-  //   localStorage.setItem("token", token);
-  //   localStorage.setItem("user", JSON.stringify(user));
-  // },
-
   login: (token, user) => {
     // ensure token is a string or null
     const tokenString = typeof token === "string" ? token : null;
